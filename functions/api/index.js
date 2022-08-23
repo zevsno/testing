190f0dc75b7dadccf9e30a969c0c4559e837c615
Zevs <zevs@tutamail.com>
2022-08-23 08:18:39 -0700

    Add UUID

--- functions/api/index.js
+++ functions/api/index.js
@@ -1,8 +1,10 @@
+import { v4 as uuidv4 } from "uuid";
+
 const jsonResponse = (value) =>
   new Response(JSON.stringify(value), {
     headers: { "Content-Type": "application/json" },
   });
 
 export const onRequest = () => {
-  return jsonResponse({ msg: "Hello World!" });
+  return jsonResponse({ msg: "Hello World!", uuid: uuidv4() });
 };
